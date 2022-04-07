a1b9cee88ef3e3bf6b4de4100f006ce9ccf81017
Muhammadjaafer <mohamadjafer14235@gmail.com>
2022-04-07 23:12:13 +0300

    test

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const quoteAuthorEl = document.querySelector("#quote--outher");
 const newQuoatBtnEl = document.querySelector("#quote--btn");
 const copyBtnEl = document.querySelector("#copy--btn");
 const saveBtnEl = document.querySelector("#save--btn");
+// >>>>>>>>>>>>>>>>>>>>>>>> //
+class App {
+  #currentQuote;
+  #currentAuthor;
+  constructor() {
+    this._getNewQuote();
+  }
+  async _getNewQuote() {
+    const res = await fetch("https://api.fisenko.net/v1/quotes/en/random");
+    const slip = await res.json();
+    console.log(slip);
+  }
+  _displayNewQuote() {}
+  _copyCurrentQuote() {}
+  _saveCurrentQuote() {}
+}
+const app = new App();
