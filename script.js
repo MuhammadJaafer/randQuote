1ae511c721ad2d7b7aad829f59054416bd6fccbb
Muhammadjaafer <mohamadjafer14235@gmail.com>
2022-04-07 21:56:20 +0300

    sellect el

---
+++ script.js
@@ -0,0 +1,5 @@
+const quoteTextEl = document.querySelector("#quote--text");
+const quoteAuthorEl = document.querySelector("#quote--outher");
+const newQuoatBtnEl = document.querySelector("#quote--btn");
+const copyBtnEl = document.querySelector("#copy--btn");
+const saveBtnEl = document.querySelector("#save--btn");
