28f58ef107a153af80256a64c0d171ef69111903
Muhammadjaafer <mohamadjafer14235@gmail.com>
2022-04-08 09:52:18 +0300

    small edit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const newQuoatBtnEl = document.querySelector("#quote--btn");
 const copyBtnEl = document.querySelector("#copy--btn");
 const saveBtnEl = document.querySelector("#save--btn");
 // >>>>>>>>>>>>>>>>>>>>>>>> //
+
 class App {
   #currentQuote;
   #currentAuthor;
