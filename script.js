3f7fde26b2930a0f6fa3a6e37c541aa0172556ea
Muhammadjaafer <mohamadjafer14235@gmail.com>
2022-04-08 14:46:22 +0300

    v 1.0

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const newQuoatBtnEl = document.querySelector("#quote--btn");
 const copyBtnEl = document.querySelector("#copy--btn");
 const saveBtnEl = document.querySelector("#save--btn");
 const copypopupEl = document.querySelector("#custom-tooltip");
+const captureEl = document.querySelector(".quote--container");
 // >>>>>>>>>>>>>>>>>>>>>>>> //
 
 class App {
@@ -13,6 +14,7 @@ class App {
     this._getNewQuote();
     newQuoatBtnEl.addEventListener("click", this._getNewQuote.bind(this));
     copyBtnEl.addEventListener("click", this._copyCurrentQuote.bind(this));
+    saveBtnEl.addEventListener("click", this._saveCurrentQuote.bind(this));
   }
   async _getNewQuote() {
     try {
@@ -39,12 +41,26 @@ class App {
     el.select();
     document.execCommand("copy");
     document.body.removeChild(el);
-
     copypopupEl.style.opacity = 100;
     setTimeout(() => {
       copypopupEl.style.opacity = 0;
     }, 1500);
   }
-  _saveCurrentQuote() {}
+  async _saveCurrentQuote() {
+    const canvas = await html2canvas(captureEl, {
+      backgroundColor: "#FFD43A",
+    });
+    const img = canvas.toDataURL("image/wpeg", 1);
+    const image = await fetch(img);
+    const imageBlog = await image.blob();
+    const imageURL = URL.createObjectURL(imageBlog);
+
+    const link = document.createElement("a");
+    link.href = imageURL;
+    link.download = this.#currentAuthor;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
 const app = new App();
